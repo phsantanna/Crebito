309698fee9b383c36dab8a8c4196870e00ea4fb1
phsantanna <paulohenriquescarrinho@gmail.com>
2024-02-22 17:59:19 -0300

    Correções

--- src/main/resources/db/migration/V1__create-transacoes-table.sql
+++ src/main/resources/db/migration/V1__create-transacoes-table.sql
@@ -1,8 +1,8 @@
-CREATE TYPE tipo_transacao as ENUM('DEBITO','CREDITO');
+CREATE TYPE tipo_transactions as ENUM('DEBITO','CREDITO');
 CREATE TABLE tb_transacoes (
                                id SERIAL PRIMARY KEY,
                                valor DOUBLE PRECISION NOT NULL,
-                               tipo tipo_transacao NOT NULL,
+                               tipo tipo_transactions NOT NULL,
                                descricao VARCHAR(255) NOT NULL,
                                realizada_em TIMESTAMP NOT NULL,
                                cliente_id BIGINT
