da3d571b7dfe4384694729e2e1261303d698be71
phsantanna <paulohenriquescarrinho@gmail.com>
2024-02-22 23:25:30 -0300

    Correção no application.properties e Dockerfile

--- src/main/java/com/controle/de/concorrencia/crebito/Crebito/CrebitoApplication.java
+++ src/main/java/com/controle/de/concorrencia/crebito/Crebito/CrebitoApplication.java
@@ -2,8 +2,9 @@ package com.controle.de.concorrencia.crebito.Crebito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CrebitoApplication {
 
 	public static void main(String[] args) {
