0dfd1eb9c17e5d70942989a4a68189d6f83b88d4
phsantanna <paulohenriquescarrinho@gmail.com>
2024-02-22 22:56:33 -0300

    Correção no application.properties

--- src/main/resources/db/migration/V1__create-transacoes-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TYPE tipo_transacao as ENUM('DEBITO','CREDITO');
-CREATE TABLE tb_transacoes (
-                               id SERIAL PRIMARY KEY,
-                               valor DOUBLE PRECISION NOT NULL,
-                               tipo tipo_transacao NOT NULL,
-                               descricao VARCHAR(255) NOT NULL,
-                               realizada_em TIMESTAMP NOT NULL,
-                               cliente_id BIGINT
-);
--- src/main/resources/db/migration/V2__create-cliente-table.sql
+++
@@ -1,6 +0,0 @@
-create table tb_cliente
-(
-    id            SERIAL PRIMARY KEY,
-    limite        double precision not null,
-    saldo_inicial double precision not null
-);
